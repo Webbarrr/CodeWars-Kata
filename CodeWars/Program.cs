2da9ea0c96235147115757758aa05babd1b83435
James Webber <James.Webber@cansfordlabs.co.uk>
2018-10-19 14:42:11 +0100

    GetVowelCount

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -10,6 +10,22 @@ namespace CodeWars
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Kata.GetVowelCount("abracadabra"));
+            Console.WriteLine(Kata.GetVowelCount("Nope!"));
+
+            Console.ReadLine();
+        }
+    }
+    public static class Kata
+    {
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+
+            // Your code here
+            char[] chars = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            foreach (char letter in str) if (chars.Contains(letter)) vowelCount++;
+            return vowelCount;
         }
     }
 }
